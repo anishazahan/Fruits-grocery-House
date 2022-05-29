a6704912a622147dc68860a774d6156e765e7ebb
anishazahan <anishazahan13@gmail.com>
2022-05-29 15:34:07 +0600

    footer update

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Footer.css'
-import { FaFacebookSquare } from "react-icons/fa";
+import { FaFacebookSquare,FaLinkedinIn,FaTwitterSquare,FaYoutubeSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className='footer-background py-5'>
         <div className="container text-color d-flex flex-row justify-content-around">
+            <div className="">
             <h4>Follow Us</h4>
             <div className="d-flex flex-row">
-                <FaFacebookSquare></FaFacebookSquare>
+                <FaFacebookSquare className='icon'></FaFacebookSquare>
+                <FaLinkedinIn className='icon'></FaLinkedinIn>
+                <FaTwitterSquare className='icon'></FaTwitterSquare>
+                <FaYoutubeSquare className='icon'></FaYoutubeSquare>
+            </div>
             </div>
             <div className=" d-flex flex-column">
                 <Link className='footer-link' to="/">Contact Us</Link>
